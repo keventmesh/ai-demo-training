012b81430d8f45cc7552ab0ab39b79dde1c59d7f
Ali Ok <aliok@redhat.com>
2023-09-28 15:23:03 +0300

    Handle the case where the prediction happens faster than client asking for a reply (#11)
    Using TTL Cache in reply service

--- services/reply-service/README.md
+++ services/reply-service/README.md
@@ -14,6 +14,8 @@ pip install -r requirements.txt
 Run:
 ```shell
 PORT=8082 \
+MAX_ITEMS_IN_CACHES=1000000 \
+CACHE_ITEM_TTL_IN_SECONDS=300 \
 python app.py
 ```
 
@@ -35,10 +37,12 @@ Run the image:
 docker run --rm \
 -p 8082:8082 \
 -e PORT="8082" \
+-e MAX_ITEMS_IN_CACHES="1000000" \
+-e CACHE_ITEM_TTL_IN_SECONDS="300" \
 ${DOCKER_REPO_OVERRIDE}/reply-service
 ```
 
-# Testing
+# Testing - case 1: prediction happens slower than client requesting reply
 
 ```shell
 # open the test UI
@@ -61,4 +65,31 @@ In UI, you will see this:
 > Received{"uploadId":"deadbeef","probability":"0.9012353451","x0":"0.24543","x1":"0.356647","y0":"0.34543","y1":"0.556647"}
 ```
 
+# Testing - case 2: prediction happens faster than client requesting reply
+
+```shell
+# open the test UI
+open localhost:8082/test
+
+# send a CloudEvent
+curl -v "http://localhost:8082" \
+  -X POST \
+  -H "Ce-Specversion: 1.0" \
+  -H "Ce-Type: demo.prediction.event" \
+  -H "Content-Type: application/json" \
+  -H "Ce-Source: knative://foo.bar" \
+  -H "Ce-time: 2020-12-02T13:49:13.77Z" \
+  -H "Ce-Id: 536808d3-88be-4077-9d7a-a3f162705f79" \
+  -d '{"uploadId":"deadbeef", "probability":"0.9012353451", "x0": "0.24543", "x1": "0.356647", "y0": "0.34543", "y1": "0.556647"}'
+
+submit the form, with value deadbeef
+  
+In UI, you will see this:
+> Received{"uploadId":"deadbeef","probability":"0.9012353451","x0":"0.24543","x1":"0.356647","y0":"0.34543","y1":"0.556647"}
+```
+
+# Testing - case 3&4: TTL
+Do the same steps as case 1 and case 2, but:
+- Start the app by lowering the TTL
+- Wait for TTL to expire
 
--- services/reply-service/app.py
+++ services/reply-service/app.py
@@ -3,10 +3,14 @@ import signal
 import sys
 from threading import Lock
 
+from cachetools import TTLCache
 from cloudevents.http import from_http
 from flask import Flask, render_template, request
 from flask_cors import CORS, cross_origin
 from flask_socketio import SocketIO
+
+MAX_ITEMS_IN_CACHES = int(os.environ.get("MAX_ITEMS_IN_CACHES", 1000 * 1000))  # 1M client erquests, 1M predictions
+CACHE_ITEM_TTL_IN_SECONDS = int(os.environ.get("CACHE_ITEM_TTL_IN_SECONDS", 5 * 60))  # 5 minutes
 
 
 def handler(signal, frame):
@@ -28,8 +32,18 @@ cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
 mutex = Lock()
-# TODO: entries here actually need a TTL to prevent leaks
-prediction_reply_requests = {}  # upload_id -> request sid (client connection id)
+
+# upload_id -> data
+replies = TTLCache(
+    maxsize=MAX_ITEMS_IN_CACHES,
+    ttl=CACHE_ITEM_TTL_IN_SECONDS
+)
+
+# upload_id -> client id (called sid in socketio)
+client_prediction_reply_requests = TTLCache(
+    maxsize=MAX_ITEMS_IN_CACHES,
+    ttl=CACHE_ITEM_TTL_IN_SECONDS
+)
 
 
 @app.route('/test')
@@ -55,24 +69,30 @@ def receive_cloud_event():
 
     upload_id = data['uploadId']
     with mutex:
-        if upload_id in prediction_reply_requests:
-            if upload_id not in prediction_reply_requests:
-                print("Upload ID not found in prediction reply requests")
-                return "Upload ID not found in prediction reply requests", 500
+        replies[upload_id] = data
 
+        # cases:
+        # 1. we just received a prediction for uploadId, and there is a client waiting for the reply
+        #   - send the reply to the client
+        #   - remove the client from the list of clients waiting for a reply
+        #   - remove the reply from replies list
+        # 2. we just received a prediction for uploadId, but there is no client waiting for the reply
+        #   - store the reply in replies list
+        #   - (in the websocket handler) when a client requests the reply, send it to the client immediately
+
+        if upload_id not in client_prediction_reply_requests:
+            print("No client waiting for reply, storing the data for future client requests")
+        else:
             # this is the websocket sid of the client waiting for the reply
-            sid = prediction_reply_requests[upload_id]
-            print("Found client waiting for reply", sid)
+            sid = client_prediction_reply_requests[upload_id]
+            print(f"Found client {sid} waiting for reply for the upload, sending the reply")
 
             # send the reply to the client, with whatever we received as the data
             socketio.emit('reply', data, room=sid)
 
             # remove the client from the list of clients waiting for a reply
-            del prediction_reply_requests[upload_id]
-
-            return "", 204
-        else:
-            print("No client waiting for reply")
+            del client_prediction_reply_requests[upload_id]
+            del replies[upload_id]
 
     return "", 204
 
@@ -86,10 +106,28 @@ def request_prediction_reply(message):
         print("No upload ID in request")
         return
 
+    # cases:
+    # 1. we already have a reply for the given uploadId:
+    #   - send the reply to the client
+    #   - remove the reply from replies list
+    #   - remove the client from the list of clients waiting for a reply (shouldn't be there, but just in case)
+    # 2. we don't have a reply for the given uploadId yet:
+    #   - store the client in the list of clients waiting for a reply
+    #   - (in the HTTP handler) when a reply is received, send it to the client immediately
+
     upload_id = message['uploadId']
     print("Requested reply for Upload ID", upload_id)
+
     with mutex:
-        prediction_reply_requests[upload_id] = request.sid
+        if upload_id in replies:
+            print(f"Found reply for upload ID {upload_id}, sending it to the client")
+            socketio.emit('reply', replies[upload_id], room=request.sid)
+            del replies[upload_id]
+            client_prediction_reply_requests.pop(upload_id, None)  # remove if exists
+        else:
+            print(f"No reply for upload ID {upload_id} yet, storing the client for future replies")
+            client_prediction_reply_requests[upload_id] = request.sid
+
     return "ok"
 
 
